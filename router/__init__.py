7243aee391310a50fbff591872e7aab1fe6f34c1
DJI28 <diogodomingues04@ua.pt>
2025-03-31 16:32:40 +0100

    Reoute modification.

--- router/__init__.py
+++ router/__init__.py
@@ -2,6 +2,6 @@ from utils.api import Router
 from .activity_registration import router as registration_router
 
 router = Router()
-router.include_router(registration_router, "/activity_registration")
+router.include_router(registration_router, "/activity-registration")
 
 __all__ = ["router"]
