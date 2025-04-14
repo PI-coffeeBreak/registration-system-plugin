16c7196cbfae57fdfcd010f3a80e6f9b138ce0d0
DJI28 <diogodomingues04@ua.pt>
2025-04-14 19:35:18 +0100

    init

--- __init__.py
+++ __init__.py
@@ -1,13 +1,31 @@
 from .router import router
 import logging
+from services.ui.plugin_settings import create_plugin_setting, delete_plugin_setting_by_title
+from schemas.plugin_setting import PluginSetting
 
 logger = logging.getLogger("coffeebreak.plugins.activity_registration")
 
-def register_plugin():
+PLUGIN_TITLE = "Activities Registration"
+PLUGIN_DESCRIPTION = "This plugin allows managing user registrations for activities with defined limits."
+
+async def register_plugin():
     logger.debug("Activity registration plugin loaded.")
 
-def unregister_plugin():
+    setting = PluginSetting(
+        title=PLUGIN_TITLE,
+        description=PLUGIN_DESCRIPTION,
+        inputs=[]
+    )
+    await create_plugin_setting(setting)
+
+    return router
+
+async def unregister_plugin():
     logger.debug("Activity registration plugin unloaded.")
+    await delete_plugin_setting_by_title(PLUGIN_TITLE)
 
 REGISTER = register_plugin
 UNREGISTER = unregister_plugin
+
+SETTINGS = {}
+DESCRIPTION = PLUGIN_DESCRIPTION
